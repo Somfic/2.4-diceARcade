9bff8b6039d64c54e6b38288bb9a73f9856d02ed
Coen <cc.denboer1@student.avans.nl>
2023-05-11 13:56:29 +0200

    added erosion and dilation

--- DiceDetection.cpp
+++ DiceDetection.cpp
@@ -11,6 +11,9 @@ using namespace std;
 
 VideoCapture cap(1);
 
+void erodeImage(Mat *originalImage, Mat *newImage);
+void dilateImage(Mat *originalImage, Mat *newImage);
+
 DiceDetection::DiceDetection() {
 
 }
@@ -35,7 +38,16 @@ int DiceDetection::startDetection()
 
 	// Threshold the grayscale image to create a binary image
 	Mat binaryImage;
-	threshold(grayImage, binaryImage, 50, 255, THRESH_BINARY);
+	
+	threshold(grayImage, binaryImage, 100, 255, THRESH_BINARY);
+
+	Mat erodedImage;
+	erodeImage(&binaryImage, &erodedImage);
+
+	Mat dilatedImage;
+	dilateImage(&binaryImage, &dilatedImage);
+
+
 
 	// Set up SimpleBlobDetector parameters
 	SimpleBlobDetector::Params params;
@@ -43,7 +55,7 @@ int DiceDetection::startDetection()
 	params.minArea = 100;
 	params.maxArea = 5000;
 	params.filterByCircularity = true;
-	params.minCircularity = 0.1;
+	params.minCircularity = 0.8;
 	params.filterByConvexity = false;
 	params.filterByInertia = false;
 
@@ -70,24 +82,20 @@ int DiceDetection::startDetection()
 
 	// Draw the keypoints on the original image
 	Mat imageWithKeypoints;
+	Mat binaryImageWithKeypoints;
+	Mat erodedImageWithKeypoints;
+	Mat dilatedImageWithKeypoints;
 	drawKeypoints(frame, keypoints, imageWithKeypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	drawKeypoints(binaryImage, keypoints, binaryImageWithKeypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	drawKeypoints(erodedImage, keypoints, erodedImageWithKeypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	drawKeypoints(dilatedImage, keypoints, dilatedImageWithKeypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
 	// Show the image with keypoints in a window
 	namedWindow("Die Image", WINDOW_NORMAL);
 	imshow("Die Image", imageWithKeypoints);
-	imshow("Die Image2", binaryImage);
-
-	// Press ESC to exit
-	//char c = waitKey(1);
-	//if (c == 27)
-	//{
-	//    break;
-	//}
-
-
-// Release the camera and close all windows
-//cap.release();
-	
+	imshow("Die Image2", binaryImageWithKeypoints);
+	imshow("eroded Image", erodedImageWithKeypoints);
+	imshow("dilated Image", dilatedImageWithKeypoints);
 
 	return keypoints.size();
 }
@@ -96,4 +104,24 @@ void DiceDetection::stop() {
 	destroyAllWindows();
 	cap.release();
 }
+
+void erodeImage(Mat *originalImage, Mat *newImage) {
+	int erosion_size = 2;
+	int erosion_type = MORPH_ELLIPSE;
+	Mat element = getStructuringElement(erosion_type,
+		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		Point(erosion_size, erosion_size));
+	erode(*originalImage, *newImage, element);
+}
+
+void dilateImage(Mat *originalImage, Mat *newImage) {
+	int dilation_type = 2;
+	int dilation_size = 5;
+	int dilation_elem = MORPH_ELLIPSE;
+	Mat element = getStructuringElement(dilation_type,
+		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+		Point(dilation_size, dilation_size));
+	dilate(*originalImage, *newImage, element);
+}
+
 
