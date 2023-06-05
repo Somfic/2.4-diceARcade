0a001281586b83b0cfc4b57c81aaa32c0c7be063
Momin2004 <m.musa@student.avans.nl>
2023-06-05 11:54:18 +0200

    imported imgui in project

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include "GameObject.h"
 #include "ObjectManager.h"
 #include <glm/gtc/matrix_transform.hpp>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 //#include "Game.h"
 #include "DiceDetection.h"
 #include <thread>
